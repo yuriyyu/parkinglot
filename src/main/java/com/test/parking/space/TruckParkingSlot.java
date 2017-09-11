60deca0ecadef4ab8da67bb700bf4e5ebf367bdd
yuriyyu <yugay.yuriy@gmail.com>
2017-09-11 10:06:33 -0500

    First commit

---
+++ src/main/java/com/test/parking/space/TruckParkingSlot.java
@@ -0,0 +1,12 @@
+package com.test.parking.space;
+
+/**
+ * Created by User on 7/4/2016.
+ */
+public class TruckParkingSlot
+        extends ParkingSlot {
+
+    public TruckParkingSlot(int slotNumber) {
+        super(slotNumber);
+    }
+}
